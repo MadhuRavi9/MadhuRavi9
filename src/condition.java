cbe4725276f394ae7644449ce432c175d2fdc1a6
Welcome <madhuribarabari@gmail.com>
2022-12-14 10:29:34 +0530

    add condition

---
+++ src/condition.java
@@ -0,0 +1,13 @@
+public class condition {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18){
+            System.out.println("You can vote");
+        }else
+            System.out.println("Try next year");
+
+
+
+    }
+
+}
