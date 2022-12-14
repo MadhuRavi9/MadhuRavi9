4394fadfceaeff77d889f77b5ccd85842ccfe3ed
Welcome <madhuribarabari@gmail.com>
2022-12-14 10:32:14 +0530

    Array

---
+++ src/jav1.java
@@ -0,0 +1,13 @@
+public class jav1 {
+    public static void main(String[] args) {
+        System.out.println("Madhu");
+        int marks = 70;
+        if (marks <35){
+            System.out.println("Fail");
+        }else if (marks >=35 && marks <50){
+            System.out.println("Pass class");
+        }else if (marks >=50 && marks <65){
+            System.out.println("First class");
+        }
+    }
+}
